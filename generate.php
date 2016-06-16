ce5caaa96eda7ff886fb930c7df6e37794ab620c
TommasoBianchi <tommaso4.bianchi@mail.polimi.it>
2016-06-16 21:55:56 +0200

    Update generate.php
    Setup database key generation and advanced query parameters

--- generate.php
+++ generate.php
@@ -1,46 +1,56 @@
 <?php
 /**
  *	This file creates the single use download link
- *	The query string should be the password (which is set in variables.php)
+ *	The query string should contain the password (which is set in variables.php)
  *	If the password fails, then 404 is rendered
  *
- *	Expects: generate.php?1234
+ *	Expects: generate.php?pw=PASSWORD&num=4&path=/secret/file&expire=2016-06-30 12:00:00&key_amount=20
+ * 		where pw is the password set in variables.php, num is the number of downloads allowed for each key,
+ * 		path is the path of the file to download, expire is the expiration date and key_amount is how many key to generate
  */
-	include("variables.php");
-	
-	// Grab the query string as a password
-	$password = trim($_SERVER['QUERY_STRING']);
 
-	// Get a human readable file size from bytes
-	function human_filesize($bytes, $decimals = 2) {
-		$sz = 'BKMGTP';
-		$factor = floor((strlen($bytes) - 1) / 3);
-		return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
-	}
+    include("variables.php");
+    include("dbconnect.php");
 	
-	/*
-	 *	Verify the admin password (in variables.php)
-	 */ 
-	if($password == ADMIN_PASSWORD) {
-		// Create a new key
-		$new = uniqid('key',TRUE);
-		
-		/*
-		 *	Create a protected directory to store keys in
-		 */
-		if(!is_dir('keys')) {
-			mkdir('keys');
-			$file = fopen('keys/.htaccess','w');
-			fwrite($file,"Order allow,deny\nDeny from all");
-			fclose($file);
-		}
-		
-		/*
-		 *	Write the key key to the keys list
-		 */
-		$file = fopen('keys/keys','a');
-		fwrite($file,"{$new}\n");
-		fclose($file);
+    // Grab the query string as an array of data
+    parse_str($_SERVER['QUERY_STRING'], $url_data);
+    
+    if(!isset($url_data['key_amount']) || $url_data['key_amount'] <= 0)
+    	$url_data['key_amount'] = 1;
+        
+    $html_data = "";
+    $keys = "";
+	
+    if(!isset($url_data['pw']) || $url_data['pw'] == ADMIN_PASSWORD) {
+      for($i = 0; $i < $url_data['key_amount']; $i++){
+        /*
+         *	Verify the admin password (in variables.php)
+         */ 
+            // Create a new key
+            $new = md5(uniqid('',TRUE));
+
+            $query = "SELECT MAX(D.id) AS max FROM `_table` AS D"; // replace _table with your actual db table
+            $result = mysql_query($query);
+            $data = mysql_fetch_array($result);
+
+            $id = intval($data['max']) + 1;
+            if(!isset($url_data['num']) || $url_data['num'] <= 0)
+                $url_data['num'] = 1;
+            if(!isset($url_data['expire']) || !(date('Y-m-d H:i:s', strtotime($url_data['expire'])) == $url_data['expire'])){
+                $date = date("Y-m-d H:i:s");
+                $date = date("Y-m-d H:i:s", strtotime($date.' '.EXPIRATION_DATE));
+            }
+            else
+                $date = $url_data['expire'];
+            if(!isset($url_data['path']))
+                $url_data['path'] = PROTECTED_DOWNLOAD;            
+
+            $query = "INSERT INTO `_table` VALUES ({$id}, '{$new}', {$url_data['num']}, 1, '{$date}', '{$url_data['path']}')"; // replace _table with your actual db table
+            mysql_query($query);
+
+            $html_data = $html_data."Key = ".$new."<br>Number of downloads = ".$url_data['num']."<br>Expiry date = ".$date."<br>Download path = ".$url_data['path']."<br><br>";
+            $keys = $keys."".$new."NEWLINE";
+      }        
 ?>
 
 <html>
@@ -53,18 +63,19 @@
 		</style>
 	</head>
 	<body>
-		<h1>Download key created</h1>
-		Your new single-use download link:<br>
+		<h1><?php echo $url_data['key_amount'] ?> Download <?php echo ($url_data['key_amount'] > 1 ? 'keys' : 'key') ?> created</h1>
+        <form action= <?php echo "\"downloadkeys.php?".$keys."\"" ?> method="post">
+        	<input type="submit" name="download_keys" value=" Download keys as a text file ">
+        </form>
 		<nl><?php 
-			echo "http://" . $_SERVER['HTTP_HOST'] . DOWNLOAD_PATH . "?" . $new; 
-			echo "<br>";
-			echo "Size: " . human_filesize(filesize(PROTECTED_DOWNLOAD), 2);
+			echo $html_data; 
 		?></nl>
 	</body>
 </html>
 
-<?php
-	} else {
+<?php    
+	} 
+    else {
 		/*
 		 *	Someone stumbled upon this link with the wrong password
 		 *	Fake a 404 so it does not look like this is a correct path
