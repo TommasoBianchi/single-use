20cf0fdbe087227a57d2fe7fdd062d7ada6061af
TommasoBianchi <tommaso4.bianchi@mail.polimi.it>
2016-06-18 14:11:02 +0200

    Update variables.php
    Variables setup

--- single-use-master/variables.php
+++ single-use-master/variables.php
@@ -8,13 +8,13 @@
  */	
 	
 	// What the file will be displayed to users as
-	define('SUGGESTED_FILENAME','prova.txt');
+	define('SUGGESTED_FILENAME','demo.txt');
 	
 	// The admin password to generate a new download link (change this as soon as you set up)
-	define('ADMIN_PASSWORD','1234');
+	define('ADMIN_PASSWORD','45813749');
 	
 	// The expiration date of the link (examples: +1 year, +5 days, +13 hours)
-	define('EXPIRATION_DATE', '+1 hours');
+	define('EXPIRATION_DATE', '+1 days');
 	
 	// Don't worry about this
 	define('CONTENT_TYPE','application/zip');
